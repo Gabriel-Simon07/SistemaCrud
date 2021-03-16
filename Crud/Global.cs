c61e5d29789c8ce81ff2f9b08f1e4be50294b610
Gabriel-Simon07 <gabrielsimon775@gmail.com>
2021-03-15 22:28:38 -0300

    Declaração de variáveis e construtores

--- Crud/Global.cs
+++ Crud/Global.cs
@@ -5,7 +5,45 @@ using System.Text;
 
 namespace Crud
 {
-    class Global
+    public class Global
     {
+        static List<Global> listaGlobal = new List<Global>();
+        protected string _Cliente;
+        protected int _Quantidade;
+        protected float _PrecoUNI;
+        protected float _totalValor;
+        protected int _pedido;
+        protected string _Produto;
+        static int _idMax;
+        protected int _id;
+
+        #region Construtores
+        public Global()
+        {
+            _Cliente = "";
+            _Quantidade = 0;
+            _PrecoUNI = 0;
+            _totalValor = 0;
+            _pedido = 0;
+            _Produto = "";
+            _idMax = 0;
+            _id = 0;
+        }
+        #endregion
+
+        #region
+        public Global(string cliente, int quantidade, float preçoUNI, float valorTotal, int pedido, string produto, int idMax, int id)
+        {
+            _Cliente = "";
+            _Quantidade = 0;
+            _PrecoUNI = 0;
+            _totalValor = 0;
+            _pedido = 0;
+            _Produto = "";
+            _idMax = 0;
+            _id = 0;
+        }
+
+        #endregion
     }
 }
