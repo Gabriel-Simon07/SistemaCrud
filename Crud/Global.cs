e028f0c6aedd3efc8d067cecee0a2991c415440d
Gabriel-Simon07 <gabrielsimon775@gmail.com>
2021-03-16 22:59:20 -0300

    Implementação de propriedades

--- Crud/Global.cs
+++ Crud/Global.cs
@@ -29,9 +29,17 @@ namespace Crud
             _idMax = 0;
             _id = 0;
         }
-        #endregion
-
-        #region
+        public Global(string cliente,  float preçoUNI, float valorTotal, int pedido, string produto, int idMax, int id)
+        {
+            _Cliente = "";
+            _Quantidade = 0;
+            _PrecoUNI = 0;
+            _totalValor = 0;
+            _pedido = 0;
+            _Produto = "";
+            _idMax = 0;
+            
+        }
         public Global(string cliente, int quantidade, float preçoUNI, float valorTotal, int pedido, string produto, int idMax, int id)
         {
             _Cliente = "";
@@ -42,6 +50,49 @@ namespace Crud
             _Produto = "";
             _idMax = 0;
             _id = 0;
+        }
+        #endregion
+
+        #region Propriedades
+        public int ID
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        public string Cliente
+        {
+            set { _Cliente= value; }
+            get { return _Cliente; }
+        }
+        public int Quantidade
+        {
+            set { _Quantidade= value; }
+            get { return _Quantidade; }
+        }
+        public float PreçoUnitario
+        {
+            set { _PrecoUNI= value; }
+            get { return _PrecoUNI; }
+        }
+        public float PrecoTotal
+        {
+            set { _totalValor= value; }
+            get { return _totalValor; }
+        }
+        public int Pedido
+        {
+            set { _pedido= value; }
+            get { return _pedido; }
+        }
+        public string Produto
+        {
+            set { _Produto= value; }
+            get { return _Produto; }
+        }
+        public int IdMax
+        {
+            set { _idMax= value; }
+            get { return _idMax; }
         }
 
         #endregion
